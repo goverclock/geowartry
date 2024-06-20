6074a7e36b59c1ff7b4f4d305278dc91a07dad88
goverclock <goverclock@outlook.com>
2024-06-20 11:43:01 +0800

    refactor select area

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,5 @@
 use bevy::{
+    ecs::query::QuerySingleError,
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
     window::PrimaryWindow,
@@ -11,7 +12,6 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<Game>()
-            .insert_resource(SelectArea(None))
             .add_systems(OnEnter(GameState::InGame), setup)
             .add_systems(OnExit(GameState::InGame), cleanup)
             .add_systems(
@@ -29,14 +29,16 @@ impl Plugin for GamePlugin {
     }
 }
 
-/// TODO: not defined yet, using usize as dummy value
+/// TODO: not defined yet
 struct Cell(());
 
 #[derive(Resource, Default)]
 struct Game {
-    board: Vec<Vec<Cell>>,          // board[r][c]
-    right_drag_start: Option<Vec2>, // window position of start of a right mouse drag, None if not dragging
-    left_drag_start: Option<Vec2>, // window position of start of a left mouse drag, None if not dragging
+    board: Vec<Vec<Cell>>, // board[r][c]
+    /// window position of start of a right mouse drag, None if not dragging
+    right_drag_start: Option<Vec2>,
+    /// window position of start of a left mouse drag, None if not dragging
+    left_drag_start: Option<Vec2>,
 }
 
 impl Game {
@@ -51,6 +53,7 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    // initialize the board
     game.board = (0..Game::BOARD_ROW)
         .map(|r| {
             (0..Game::BOARD_COLUMN)
@@ -198,51 +201,60 @@ fn to_menu_on_return(
     }
 }
 
-#[derive(Resource)]
-struct SelectArea(Option<Entity>);
+#[derive(Component, Debug)]
+struct SelectArea {
+    /// world coord of select area start
+    start: Vec2,
+    /// world coord of select area end
+    end: Vec2,
+}
 
-/// spawn/despawn selected area in the world
-/// any selectable entities colliding with it will be selected
+// /// spawn/despawn selected area in the world
+// /// any selectable entities colliding with it will be selected
 fn select_area(
     mut cmds: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    mut query_area: Query<(&mut Transform, &mut Mesh2dHandle)>,
     camera_tf: Query<(&Camera, &GlobalTransform)>,
-    mut cursor_moved_events: EventReader<CursorMoved>,
-    mut select_area: ResMut<SelectArea>,
+    window: Query<&Window, With<PrimaryWindow>>,
+    mut query_select_area: Query<(Entity, &mut Transform, &mut Mesh2dHandle, &mut SelectArea)>,
     game: ResMut<Game>,
 ) {
+    // not dragging select, despawn the rectangle area if exist
     if let None = game.left_drag_start {
-        // not dragging select, despawn the rectangle area if exist
-        if let Some(area) = select_area.0 {
-            cmds.entity(area).despawn();
-            select_area.0 = None;
-            info!("despawned select area");
+        match query_select_area.get_single() {
+            Ok((e, _, _, area)) => {
+                info!("selected area {:?} ", area);
+                cmds.entity(e).despawn();
+            }
+            Err(QuerySingleError::NoEntities(_)) => {}
+            Err(QuerySingleError::MultipleEntities(_)) => {
+                unreachable!("should only have at most one select area")
+            }
         }
         return;
     }
 
     // dragging
-    // spawn the rectangle area if not yet
+    // get drag start and current window position
     let start = game.left_drag_start.unwrap();
-    let mut cur = start;
-    for e in cursor_moved_events.read() {
-        // TODO: this may conflict with mouse_move_view's .read()
-        cur = e.position;
-    }
-    if cur == start {
+    let cur = window.single().cursor_position();
+    if let None = cur {
         return;
     }
+    let cur = cur.unwrap();
+
+    // turn drag start/end window position into world coords
     let (cam, cam_tf) = camera_tf.single();
     let start = window_to_world_coords(start, cam, cam_tf);
     let cur = window_to_world_coords(cur, cam, cam_tf);
     if start.is_none() || cur.is_none() {
         info!("cursor position to world is None, not handled for selecting area");
     }
-
     let start_world_coord = start.unwrap();
     let cur_world_coord = cur.unwrap();
+
+    // the transform of a rect is the center of it
     let middle_world_coord = Vec2::new(
         (cur_world_coord.x + start_world_coord.x) / 2.0,
         (cur_world_coord.y + start_world_coord.y) / 2.0,
@@ -251,22 +263,32 @@ fn select_area(
     let area_width = (start_world_coord.y - cur_world_coord.y).abs();
     let shape = Mesh2dHandle(meshes.add(Rectangle::new(area_length, area_width)));
 
-    if let Some(area) = select_area.0 {
-        let (mut tf, mut mesh_handle) = query_area.get_mut(area).unwrap();
-        *tf = Transform::from_xyz(middle_world_coord.x, middle_world_coord.y, 2.0);
-        *mesh_handle = shape;
-        // info!("changed area to {} {}", start_world_coord, cur_world_coord);
-    } else {
-        let e = cmds
-            .spawn(MaterialMesh2dBundle {
-                mesh: shape,
-                material: materials.add(Color::rgba(0.0, 0.3, 0.0, 0.5)),
-                transform: Transform::from_xyz(start_world_coord.x, start_world_coord.y, 2.0),
-                ..default()
-            })
-            .id();
-        select_area.0 = Some(e);
-        info!("created area {} {}", start_world_coord, cur_world_coord);
+    // update transform of the rect if it exists,
+    // else spawn it
+    match query_select_area.get_single_mut() {
+        Ok((_, mut tf, mut mesh, mut area)) => {
+            *tf = Transform::from_xyz(middle_world_coord.x, middle_world_coord.y, 2.0);
+            *mesh = shape;
+            area.start = start_world_coord;
+            area.end = cur_world_coord;
+        }
+        Err(QuerySingleError::NoEntities(_)) => {
+            cmds.spawn((
+                MaterialMesh2dBundle {
+                    mesh: shape,
+                    material: materials.add(Color::rgba(0.0, 0.3, 0.0, 0.5)), // color of select area, semi transparent
+                    ..default()
+                },
+                SelectArea {
+                    start: start_world_coord,
+                    end: cur_world_coord,
+                },
+            ));
+            info!("select area spawned");
+        }
+        Err(QuerySingleError::MultipleEntities(_)) => {
+            unreachable!("should only have at most one select area")
+        }
     }
 }
 
